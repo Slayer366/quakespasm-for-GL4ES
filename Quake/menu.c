003c3f1bab9b949165204e4e0e590c10befbf196
Slayer366 <andykratz@hotmail.com>
2023-10-22 17:24:42 -0500

    Fix most endgame/text/messages not being displayed

--- Quake/menu.c
+++ Quake/menu.c
@@ -1706,9 +1706,17 @@ void M_Quit_Draw (void) //johnfitz -- modified for new quit message
 	M_DrawTextBox	(160-4*(boxlen+2), 76, boxlen, 4);
 
 	//now do the text
+#ifdef USE_GL4ES
+	glEnable(GL_BLEND);
+#endif
+
 	M_Print			(160-4*(sizeof(msg1)-1), 88, msg1);
 	M_Print			(160-4*(sizeof(msg2)-1), 96, msg2);
 	M_PrintWhite		(160-4*(sizeof(msg3)-1), 104, msg3);
+
+#ifdef USE_GL4ES
+	glDisable(GL_BLEND);
+#endif
 }
 
 //=============================================================================
